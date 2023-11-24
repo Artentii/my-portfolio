66e84a9e128e37e6e7aefed5eb00f2bea06bb140
Marina Terentii <125751323+Artentii@users.noreply.github.com>
2023-11-24 10:20:27 +0100

    Edited style services section

--- src/app/components/services.js
+++ src/app/components/services.js
@@ -5,7 +5,7 @@ const addDigitalPresenceCards = [
     imgUrl: "/services_first.png",
     textAlt: "Image one",
     title: "Creating a great first impression",
-    text: "A captivating digital presence is the initial point of contact with your audience. Aa a UX engineer I’ll ensure a user-friendly and intuitive experience.",
+    text: "A captivating digital presence is the initial point of contact with your audience. As a UX engineer I’ll ensure a user-friendly and intuitive experience.",
   },
   {
     imgUrl: "/services_second.png",
@@ -23,20 +23,19 @@ const addDigitalPresenceCards = [
 
 export default function Services() {
   return (
-    <div>
-      <h3>Services</h3>
-      <h4>Elevate your digital presence</h4>
-      <p>
-        Your digital presence serves as the digital storefront, the first point of contact for your audience. It's the online space where potential customers and visitors get their first glimpse of
-        your brand, products, or services.
+    <div className="bg-orange-300">
+      <h3 className="text-6xl font-semibold text-center py-2">Services</h3>
+      <h4 className="text-4xl font-semibold text-center py-2" style={{ color: "var(--cadmiumOrange)"}}>Elevate your digital presence</h4>
+      <p className="text-center lg:text-lg lg:w-1/2 mx-auto p-2">
+        Your digital presence serves as the digital storefront, the first point of contact for your audience. It&apos;s the online space where potential customers and visitors get their first glimpse
+        of your brand, products, or services. So, how can I help you in achieving a remarkable digital presence?
       </p>
-      <p>So, how can I help you in achieving a remarkable digital presence?</p>
-      <div className="flex w-10/12 bg-orange-200 mx-auto p-2">
+      <div className="flex flex-col lg:flex-row bg-orange-200 mx-auto p-2">
         {addDigitalPresenceCards.map((card, index) => (
-          <div className="bg-orange-400 m-1 w-[400px] mx-auto p-4" key={index}>
-            <Image src={card.imgUrl} alt={card.textAlt} className="dark:invert mx-auto" width={180} height={100} priority />
-            <p>{card.title}</p>
-            <p>{card.text}</p>
+          <div className="m-1 lg:w-2/6 p-4 rounded-xl" key={index} style={{ background: "var(--antiqueWhite)", boxShadow: "var(--shadowCardService)", border: "var(--borderCardService)" }}>
+            <Image src={card.imgUrl} alt={card.textAlt} className="mx-auto" width={180} height={100} priority />
+            <p className="text-black text-2xl font-semibold py-2 text-center" style={{ color: "var(--cadmiumOrange)"}}>{card.title}</p>
+            <p className="text-black text-center">{card.text}</p>
           </div>
         ))}
       </div>
