37459caffb53bdc054f2abd224bc990bcfdf8789
Marina Terentii <125751323+Artentii@users.noreply.github.com>
2023-12-04 12:38:34 +0100

    Added style to footer section

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import Services from "../../src/app/components/services";
 import Projects from "../../src/app/components/projects";
 import About from "../../src/app/components/about";
 import Contact from "./components/contact";
+import Footer from "./components/footer"
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <Projects />
       <About />
       <Contact />
+      <Footer />
     </main>
   );
 }
