80312fac2a94b1d80f7a98d9d3b20e663d2da339
Marina Terentii <125751323+Artentii@users.noreply.github.com>
2023-12-05 14:50:05 +0100

    Added style to latest projects section

--- src/app/page.js
+++ src/app/page.js
@@ -4,11 +4,13 @@ import Projects from "../../src/app/components/projects";
 import About from "../../src/app/components/about";
 import Contact from "./components/contact";
 import Footer from "./components/footer"
+import LatestProjects from "./components/latestprojects";
 
 export default function Home() {
   return (
     <main className="bg-pale-page">
       <div className="noise"></div>
+      <LatestProjects/>
       <Services />
       <Projects />
       <About />
