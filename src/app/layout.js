e50972ede64e4a1517b3a400cb0fde5c7fe06fab
Marina Terentii <125751323+Artentii@users.noreply.github.com>
2024-02-05 11:18:25 +0100

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Outfit } from "next/font/google";
 import Providers from "./providers";
 import { Analytics } from "@vercel/analytics/react";
+import Head from "next/head";
 
 // Components
 import NavBar from "./components/base/navbar";
@@ -13,20 +14,6 @@ export const metadata = {
   title: "Graphic Design | Brand Identity | Web Design & Dev - Marina Terentii",
   description: "Ready to bring your ideas to life? Transform your brand's visual identity and elevate your online game. Your vision, our design - let's make it happen.",
   keywords: "Visual Identity, Graphic Designer, Branding, Logo Design, Web Design",
-  openGraph: {
-    type: "website",
-    url: "https://www.artentii.com/",
-    title: "Graphic Design | Brand Identity | Web Design & Dev - Marina Terentii",
-    description: "Ready to bring your ideas to life? Transform your brand's visual identity and elevate your online game. Your vision, our design - let's make it happen.",
-    images: [{ url: "/opengraph-image.jpg", width: 1200, height: 630 }],
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "Graphic Design | Brand Identity | Web Design & Dev - Marina Terentii",
-    description: "Ready to bring your ideas to life? Transform your brand's visual identity and elevate your online game. Your vision, our design - let's make it happen.",
-    creator: "@Artentii",
-    images: [{ url: "/opengraph-image.jpg", width: 1200, height: 630 }],
-  },
 };
 
 const outfit = Outfit({ subsets: ["latin"] });
@@ -34,6 +21,23 @@ const outfit = Outfit({ subsets: ["latin"] });
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={outfit.className}>
+      <Head>
+        <title>Graphic Design | Brand Identity | Web Design & Dev - Marina Terentii</title>
+        <meta name="description" content="Ready to bring your ideas to life? Transform your brand's visual identity and elevate your online game. Your vision, our design - let's make it happen." />
+        <meta name="keywords" content="Visual Identity, Graphic Designer, Branding, Logo Design, Web Design" />
+        <meta property="og:url" content="https://www.artentii.com/"/>
+        <meta property="og:type" content="website"/>
+        <meta property="og:title" content="Graphic Design | Brand Identity | Web Design & Dev - Marina Terentii"/>
+        <meta property="og:description" content="Ready to bring your ideas to life? Transform your brand's visual identity and elevate your online game. Your vision, our design - let's make it happen."/>
+        <meta property="og:image" content="https://my-portfolio-ld737ne0s-artentii.vercel.app/opengraph-image.jpg?833af47f10b26bb6"/>
+
+        <meta name="twitter:card" content="summary_large_image"/>
+        <meta property="twitter:domain" content="artentii.com"/>
+        <meta property="twitter:url" content="https://www.artentii.com/"/>
+        <meta name="twitter:title" content="Graphic Design | Brand Identity | Web Design & Dev - Marina Terentii"/>
+        <meta name="twitter:description" content="Ready to bring your ideas to life? Transform your brand's visual identity and elevate your online game. Your vision, our design - let's make it happen."/>
+        <meta name="twitter:image" content="https://my-portfolio-ld737ne0s-artentii.vercel.app/opengraph-image.jpg?833af47f10b26bb6"/>
+      </Head>
       <body className="bg-light dark:bg-dark text-gray dark:text-white">
         <Providers>
           <div className="noise"></div>
